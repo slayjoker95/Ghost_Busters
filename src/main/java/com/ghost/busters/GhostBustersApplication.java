2b7ae7b042e082a228b0c5b8a95752c224ef5833
slayjoker95 <slayjoker95@gmail.com>
2018-01-19 12:34:19 +0300

    Initial commit

---
+++ src/main/java/com/ghost/busters/GhostBustersApplication.java
@@ -0,0 +1,18 @@
+package com.ghost.busters;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class GhostBustersApplication extends SpringBootServletInitializer {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GhostBustersApplication.class, args);
+	}
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(GhostBustersApplication.class);
+	}
+}
